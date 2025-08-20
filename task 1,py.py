3a386b4bc332df35eb6ed37a4c3fc1dce76fa5c8
sgshubham728-rgb <sgshubham728@gmail.com>
2025-08-20 14:07:39 +0530

    Add files via upload

---
+++ task 1,py.py
@@ -0,0 +1,13 @@
+n1=input("Enter your first number:")
+n2=input("Enter your second number:")
+n1=int(n1)
+n2=int(n2)
+a1=n1+n2
+s1=n1-n2
+m1=n1*n2
+d1=n1/n2
+print("addition:",a1)
+print("subtraction:",s1)
+print("multiplication:",m1)
+print("division:",d1)
+      
